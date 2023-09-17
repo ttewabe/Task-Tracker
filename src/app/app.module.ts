9985c20ca57b09f4142ab6256349eb9b63029327
Tewabe Tadege <ethiohabesha412@gmail.com>
2023-09-17 16:58:29 -0700

    added task-item

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ButtonComponent } from './components/button/button.component';
 import { TasksComponent } from './components/tasks/tasks.component';
+import { TaskItemComponent } from './components/task-item/task-item.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { TasksComponent } from './components/tasks/tasks.component';
     HeaderComponent,
     ButtonComponent,
     TasksComponent,
+    TaskItemComponent,
   ],
   imports: [
     BrowserModule,
